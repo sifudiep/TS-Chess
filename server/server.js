779b46deb72b527a253b1c37174888d2b880c189
sifudiep <code.sifudiep@gmail.com>
2021-08-01 23:41:35 +0200

    change to process.env.PORT for heroku app

--- server/server.js
+++ server/server.js
@@ -13,4 +13,4 @@ io.on('connection', (socket) => {
     })
 })
 
-http.listen(8080, () => console.log('listening localhost:8080'));
+http.listen(process.env.PORT, () => console.log('listening localhost:8080'));
